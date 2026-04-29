38acef05c1ad6b265d06cfc1dbcb3b184cc9a6c8
ka1sser <serenokaisser.dev@gmail.com>
2026-04-30 00:21:26 +0800

    Added animation for the cards and Testimonial section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,10 +1,13 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import TestimonialCard from './TestimonialCard'
 import { testimonials } from '../../constants'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const Testimonials = () => {
 
   const [currentIndex, setCurrentIndex] = useState(0);
+  const containerRef = useRef();
 
   const handlePrev = () => {
     if (currentIndex > 0) {
@@ -18,11 +21,51 @@ const Testimonials = () => {
     }
   }
 
+  useGSAP(() => {
+    const cards = containerRef.current.children;
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#working-area",
+        start: "top center",
+        end: "bottom bottom",
+        scrub: 1,
+        snap: {
+          snapTo: 0.1,
+          directional: false,
+          duration: 0.5,
+          ease: "power1.inOut",
+        },
+      },
+    });
+
+    tl.fromTo(
+      "#working-area h1",
+      { opacity: 0, y: 50 },
+      { opacity: 1, y: 0, duration: 1, ease: "power1.inOut" },
+    )
+      .fromTo(
+        "#working-area .container",
+        { opacity: 0, y: 50 },
+        { opacity: 1, y: 0, duration: 1, ease: "power1.inOut" },
+      )
+      .fromTo(
+        cards,
+        { opacity: 0 },
+        { opacity: 1, stagger: 0.1, ease: "power1.inOut" },
+      )
+      .fromTo(
+        ".arrows",
+        { opacity: 0 },
+        { opacity: 1, stagger: 0.1, ease: "power1.inOut" },
+        "<",
+      );
+  }, [currentIndex]);
+
   return (
     <section id="testimonials">
       <div id="working-area">
         <h1>THEIR EXPERIENCES</h1>
-        <div className="container">
+        <div className="container" ref={containerRef}>
           {testimonials
             .slice(currentIndex, currentIndex + 3)
             .map(({ imgPath, testimonial, user, business }, index) => (
@@ -37,10 +80,21 @@ const Testimonials = () => {
         </div>
         <div className="arrows">
           <button onClick={handlePrev} disabled={currentIndex === 0}>
-            <img src="/images/left-arrow.svg" />
+            <img
+              src="/images/left-arrow.svg"
+              alt="left-arrow"
+              aria-hidden="true"
+            />
           </button>
-          <button onClick={handleNext} disabled={currentIndex >= testimonials.length - 3}>
-            <img src="/images/right-arrow.svg" />
+          <button
+            onClick={handleNext}
+            disabled={currentIndex >= testimonials.length - 3}
+          >
+            <img
+              src="/images/right-arrow.svg"
+              alt="right-arrow"
+              aria-hidden="true"
+            />
           </button>
         </div>
       </div>
